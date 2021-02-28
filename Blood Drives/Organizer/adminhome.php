b58fe87062e1ebeb6d9b082c4198da838a506238
Charulatha <61926985+charu-19@users.noreply.github.com>
2021-02-28 12:02:19 +0530

    Add files via upload
    Hack-a-sol Hackathon

---
+++ Blood Drives/Organizer/adminhome.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin </title>
+    <link rel="stylesheet" href="adminhome.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <input type="checkbox" id="check">
+    <label for="check">
+      <i class="fas fa-bars" id="btn"></i>
+      <i class="fas fa-times" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+    <header>Dashboard</header>
+  <ul>
+    <li><a href="appoinments.php"><i class="fas fa-images"></i> Appointments</a></li>
+    <li><a href="addblooddrives.php"><i class="fas fa-plus"></i>Add Blood Drives</a></li>
+
+  </ul>
+</div>
+ <section>
+     <div class="cont">
+      <h1><i>Donate blood..Save Life<br><br></i></h1>
+      <img src="https://imgeng.jagran.com/images/2020/jun/14_06_2020-blood_donation_203828111592103404085.jpg" height="600" width="600">
+         </div>
+        </section>
+  </body>
+</html>
---
+++ Blood Drives/User/login/booking.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+	
+$con = mysqli_connect('localhost','root','');
+mysqli_select_db($con,'sangamdb');
+
+$username = $_POST['Name'];
+$email = $_POST['Email'];
+$phone_number = $_POST['Phone_no'];
+$blood_group = $_POST['Blood_group'];
+$location = $_POST['location'];
+
+// Create connection
+
+$s = "select * from booking where Email = '$email'";
+$result = mysqli_query($con,$s);
+$num = mysqli_num_rows($result);
+if($num == 1){
+   echo "email already exixts";
+ }
+
+else{
+$reg = "INSERT INTO booking (Name, Email, Phone_no, Blood_group, location)values ('$username','$email','$phone_number','$blood_group','$location')";
+ mysqli_query($con,$reg);
+   echo "<script language='javascript'>"; 
+     echo "alert('Registered Sucessfully')";
+     echo "</script>";  
+ }
+?>
+
+ 
